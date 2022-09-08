b507d5f740cabc8308f390213a6adc136b41327f
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-08 13:13:04 -0700

    hello_world

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,23 @@
+i#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("Holberton SchoolGCC");
+
+			return (0);
+
+}
