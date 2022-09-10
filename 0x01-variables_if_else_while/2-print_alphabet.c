ef1aab8f88767f55ff848f8fa66552916a096edb
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-09 19:23:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char oluwatosin;
+
+	for (oluwatosin = 'a'; oluwatosin <= 'z'; oluwatosin++)
+	{
+		putchar(oluwatosin);
+	}
+	putchar('\n');
+	return (0);
+}
