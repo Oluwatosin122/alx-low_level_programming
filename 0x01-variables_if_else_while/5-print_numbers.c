83420889bdbab5301392fbfe020190b814d784cf
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-09 19:53:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main _ Entry point
+ * author: oluwatosin
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int oluwatosin;
+
+	for (oluwatosin = 0; oluwatosin < 10; oluwatosin++)
+	{
+		printf("%d", oluwatosin);
+	}
+	printf("\n");
+	return (0);
+}
