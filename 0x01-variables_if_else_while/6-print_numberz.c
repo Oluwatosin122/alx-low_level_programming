698b6879dd8d7a17a6e484c4e00271d56e6dd2fc
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-09 20:06:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: oluwatosin
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int oluwatosin
+
+	for (oluwatosin = 48; oluwatosin < 58; oluwatosin++)
+		putchar(oluwatosin);
+	putchar('\n');
+	return (0);
+}
