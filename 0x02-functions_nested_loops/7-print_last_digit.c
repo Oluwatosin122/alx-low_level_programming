b8ace056febcfa873aacfe7fb923a35c6e6a98f8
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-14 16:11:35 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,16 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digits of any number
- * @n: the int value to be acted upon
+ * @c: the int value to be acted upon
  * author: oluwatosin
  * Return: returns the last digit to calling function
  */
 int print_last_digit(int c)
 {
-	if (c > 9 || c == 0)
-	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
-	else
-	{
-	c = c * -1;
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
+	c %= 10;
+	if (c < 0)
+		c *= -1;
+	_putchar(c + 0);
+	return (c);
+
 }
