0922b2bd3c585cb7eddc056a630dc9e2e79a0c67
Oluwatosin122 <gentledove22@yahoo.co.uk>
2022-09-19 17:30:24 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,8 @@
 
 /**
  * swap_int - This function swaps the value of two integers
- * @a: a pointer dereference for changing the value of the variable b
+ * @a: a pointer dereference for changing the value of the variable a
+ * @b: a pointer deference for changing the value of the variable b
  * int c: assigning the address of the variable a to c
  * Author: Oluwatosin
  * Return 0 success.
